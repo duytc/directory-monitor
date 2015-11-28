33f63e78bba8327db954fb58270f1fa858fd8a7f
dunght160387 <dunght1603@gmail.com>
2015-11-28 15:11:04 +0700

    Monitor root dir, get publisherId from new File path

--- run.php
+++ run.php
@@ -9,7 +9,7 @@ use React\EventLoop\Factory as EventLoopFactory;
 use src\Worker;
 
 $loop = EventLoopFactory::create();
-$monitor = new RecursiveMonitor($loop, __DIR__ . '/data/2');
+$monitor = new RecursiveMonitor($loop, __DIR__ . '/data');
 
 /* Ignore all temp files: */
 $monitor->ignore('\___jb_');
@@ -18,9 +18,11 @@ $monitor->ignore('\___jb_');
 $monitor->on('create', function ($path, $root) {
     echo "Got new file: {$path} in {$root}\n";
 
-    $filePath = $root . '/' . $path;
+    $fileName = basename($path);
+    $dir = $root . '/' . substr($path, 0, strpos($path, $fileName) - 1); // not include '/' at the end
+    $filePath = $dir . '/' . $fileName;
 
-    (new Worker())->doJob($root, $filePath);
+    (new Worker())->doJob($dir, $filePath);
 });
 
 $loop->run();

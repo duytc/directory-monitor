ea60cb65fae87d45019c1d1a28adaac32bfb288f
dunght160387 <dunght1603@gmail.com>
2015-11-30 15:41:57 +0700

    Add AdNetwork directory level before publisher

--- src/Worker.php
+++ src/Worker.php
@@ -4,6 +4,8 @@ namespace src;
 
 class Worker
 {
+    const DIR_MIN_DEPTH_LEVELS = 2; // <root>/<adNetworkName>/<publisherId>, e/g: data/Pulse Point/2
+
     public function doJob($dir, $filePath)
     {
         if (!$this->isFileValid($filePath)) {
@@ -11,14 +13,31 @@ class Worker
         }
 
         // build commands importing file
-        $publisherId = filter_var(basename($dir), FILTER_VALIDATE_INT);
-        if (!$publisherId) {
-            throw new \Exception(sprintf("Can not extract publisherId from dir %s!!!\n", $dir));
+        $dirs = explode('/', $dir);
+        if(!is_array($dirs) || count($dirs) < self::DIR_MIN_DEPTH_LEVELS) {
+            throw new \Exception("Expect dir has depth levels >= 2!!!\n");
         }
 
-        $command = sprintf('php %s/app/console tc:unified-report:import --publisher %s %s', TAGCADE_UNIFIED_REPORT_IMPORT_MODULE, $publisherId, $filePath);
+        $publisherId = filter_var(array_pop($dirs), FILTER_VALIDATE_INT);
+        if (!$publisherId) {
+            throw new \Exception(sprintf("Can not extract Publisher from dir %s!!!\n", $dir));
+        }
 
-        echo sprintf("importing File %s with Publisher #%s ...\n", $filePath, $publisherId);
+        $adNetworkName = array_pop($dirs);
+        if (empty($adNetworkName)) {
+            throw new \Exception(sprintf("Can not extract AdNetwork from dir %s!!!\n", $dir));
+        }
+
+        $command = sprintf("php \"%s/app/console\" tc:unified-report:import --publisher %s --network \"%s\" \"%s\"",
+            TAGCADE_UNIFIED_REPORT_IMPORT_MODULE, // make between "" because it may contain space...
+            $publisherId,
+            $adNetworkName,
+            $filePath
+        );
+
+        echo sprintf("importing File %s with Publisher #%s and AdNetwork %s...\n", $filePath, $publisherId, $adNetworkName);
+
+        echo sprintf("command %s\n", $command);
 
         // You may use status(), start(), and stop(). notice that start() method gets called automatically one time.
         $process = new Process($command);
